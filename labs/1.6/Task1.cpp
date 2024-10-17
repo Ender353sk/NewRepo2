38179c690c38e4868c188071311e26b7aaaa9929
Ender353sk <ender3597@gmail.com>
2024-10-17 20:54:26 +0300

    dergbdf

---
+++ labs/1.6/Task1.cpp
@@ -0,0 +1,17 @@
+/*
+    Student: Yakymenko Artur
+    Group: 123
+    Lab 1.6
+*/
+
+int main() {
+    const char CONST1 = '5';
+    char chVar1 = 'b';
+    char chVar2;
+    chVar2 = '!';
+
+    const char CONST2 = 0xc;
+    char chVar3 = 0x68;
+    char chVar4 = 0x2b;
+    return 0;
+}
