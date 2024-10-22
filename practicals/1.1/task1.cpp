b4d5ca1e8020e6cc2c48078d88a38ee890eef59c
Ender353sk <ender3597@gmail.com>
2024-10-22 15:58:36 +0300

    tydhftyd

---
+++ practicals/1.1/task1.cpp
@@ -0,0 +1,18 @@
+/*
+	Done by:
+	Student: Yakymenko Artur
+	Group: 123
+	Practical 1
+*/
+//task1
+#include <iostream>
+using namespace std;
+int main() {
+	double a, b;
+	cout << "1 number: ";
+	cin >> a;
+	cout << "2 number: ";
+	cin >> b;
+	auto q = (a + b) / 2;
+	cout << q << endl;
+}
