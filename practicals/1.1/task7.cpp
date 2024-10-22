04e5a201dc114fbfdbb9a621db62565e1aa71d73
Ender353sk <ender3597@gmail.com>
2024-10-22 20:55:06 +0300

    grtahgrt

---
+++ practicals/1.1/task7.cpp
@@ -0,0 +1,18 @@
+/*
+	Done by:
+	Student: Yakymenko Artur
+	Group: 123
+	Practical 1
+*/
+//task7
+#include <iostream>
+using namespace std;
+int main() {
+	double a, b, c, d, e;
+	a = 1;
+	b = 2;
+	c = 6;
+	d = 24;
+	e = 120;
+	cout << (a + b + c + d + e) / 5 << endl;
+}
