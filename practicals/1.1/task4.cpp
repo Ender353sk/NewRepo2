6bfb2cdf433b0f6d46567af02be61ffc840eb7f1
Ender353sk <ender3597@gmail.com>
2024-10-22 16:11:51 +0300

    tdhtbtr

---
+++ practicals/1.1/task4.cpp
@@ -0,0 +1,18 @@
+/*
+    Done by:
+    Student: Yakymenko Artur
+    Group: 123
+    Practical 1
+*/
+//task 4
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "¬вед≥ть число a: ";
+    cin >> a;
+    a = -a;
+    cout << a << endl;
+    return 0;
+}
