507061f4f71d8a061024cc8fef061244239a9e64
Ender353sk <ender3597@gmail.com>
2024-10-22 16:05:21 +0300

    ghfbdtf

---
+++ practicals/1.1/task3.cpp
@@ -0,0 +1,19 @@
+/*
+	Done by:
+	Student: Yakymenko Artur
+	Group: 123
+	Practical 1
+*/
+//task3
+#include <iostream>
+using namespace std;
+int main() {
+	double a, b;
+	cout << "1 number: ";
+	cin >> a;
+	cout << "2 number: ";
+	cin >> b;
+	auto x = a - b;
+	auto y = a / b;
+	cout << "Різниця: " << x << " Частка " << y << endl;
+}
