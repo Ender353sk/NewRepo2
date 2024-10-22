578a80bb870fc61a052317eb16677b8346337ab1
Ender353sk <ender3597@gmail.com>
2024-10-22 16:16:35 +0300

    rghsgr

---
+++ practicals/1.1/task5.cpp
@@ -0,0 +1,19 @@
+/*
+	Done by:
+	Student: Yakymenko Artur
+	Group: 123
+	Practical 1
+*/
+//task5
+#include <iostream>
+using namespace std;
+int main() {
+	double a, b, c;
+	cout << "1 number: ";
+	cin >> a;
+	cout << "2 number: ";
+	cin >> b;
+	cout << "3 number: ";
+	cin >> c;
+	cout << c * (a + b) << endl;
+}
